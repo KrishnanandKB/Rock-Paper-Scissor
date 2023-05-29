8ec65becf10b13ecc44b4f7f512c9185215e9b43
Krishnanand K B <krishnanandavittam@gmail.com>
2023-05-29 23:17:45 +0530

    added the playes and computer choice

--- index.js
+++ index.js
@@ -6,3 +6,30 @@ const choiceBtn = document.querySelectorAll(".choiceBtn");
 let player;
 let computer;
 let result;
+
+choiceBtn.forEach(button => button.addEventListener("click",()=>{
+    player = button.textContent;
+    computerTurn();
+    playerText.textContent = `Player: ${player}`;
+    computerText.textContent = `Computer: ${computer}`;
+    resultText.textContent = checkWinner ()
+
+
+
+}));
+function computerTurn(){
+    const randNum = Math.floor(Math.random()* 3) + 1;
+
+    switch(randNum){
+        case 1:
+        computer = "ROCK";
+        break;
+        case 2:
+        computer = "PAPER";
+        break;
+        case 3:
+        computer = "SCISSOR" ;
+        break;
+
+    }
+}
