0b0b41317e7d74eab3bd2767498e555c0ec9c279
Krishnanand K B <krishnanandavittam@gmail.com>
2023-05-29 23:13:50 +0530

    added all the elements that we need

---
+++ index.js
@@ -0,0 +1,8 @@
+const playerText = document.querySelector("#playerText");
+const computerText = document.querySelector("#computerText");
+const resultText = document.querySelector("#resultText");
+const choiceBtn = document.querySelectorAll(".choiceBtn");
+
+let player;
+let computer;
+let result;
